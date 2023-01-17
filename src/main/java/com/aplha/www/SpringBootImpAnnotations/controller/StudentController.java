5f88c2e1bb1ac7b716c90df320ec45dee362df55
NM <masihnishit@gmail.com>
2023-01-17 22:16:23 +0530

    video 24

--- src/main/java/com/aplha/www/SpringBootImpAnnotations/controller/StudentController.java
+++ src/main/java/com/aplha/www/SpringBootImpAnnotations/controller/StudentController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -30,8 +31,8 @@ public class StudentController {
 		List<Student> students=new ArrayList<>();
 		students.add(new Student(1, "tom", "sharma"));
 		students.add(new Student(2, "jerry", "agrawal"));
-		students.add(new Student(1, "tony", "stark"));
-		students.add(new Student(1, "john", "carter"));
+		students.add(new Student(3, "tony", "stark"));
+		students.add(new Student(4, "john", "carter"));
 		return students;
 	}
 	
@@ -62,12 +63,21 @@ public class StudentController {
 		return new Student(id, firstName, lastName);
 	}
 	
-	// spring boot REST API that handles HTTP POST request
+	// spring boot REST API that handles HTTP POST request - creating new resource
 	// http://localhost:8080/students/create
 	@PostMapping("students/create")
 	@ResponseStatus(HttpStatus.CREATED)
 	public Student createStudent(@RequestBody Student student) {
 		System.out.println(student.getId());
+		System.out.println(student.getFirstName());
+		System.out.println(student.getLastName());
+		return student;
+	}
+	
+	// spring boot REST API that handles HTTP PUT request - updating existing resource
+	// http://localhost:8080/students/5/update
+	@PutMapping("students/{id}/update")
+	public Student updateStudent(@RequestBody Student student, @PathVariable("id") int studentId) {
 		System.out.println(student.getFirstName());
 		System.out.println(student.getLastName());
 		return student;
